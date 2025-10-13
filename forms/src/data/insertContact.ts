5add16af96a5c7a0fc519183ffc3b1561b6c6c65
alexander todorovic <alexanderttodorovic@gmail.com>
2025-10-13 23:39:54 +0200

    Refactor code structure for improved readability and maintainability

--- forms/src/data/insertContact.ts
+++ forms/src/data/insertContact.ts
@@ -1,35 +1,44 @@
 'use server';
 
 import {
-    createClient,
-    type Client,
+  createClient,
+  type Client,
 } from '@libsql/client';
-
 import { redirect } from 'next/navigation';
-import { Contact } from './schema';
+import { contactSchema } from './schema';
 
-export async function insertContact(formData: FormData) {
-    const { name, email, reason, notes } =
-        Object.fromEntries(formData) as Contact;
-    let client: Client | undefined;
-    let ok = true;
-    try {
-        client = createClient({
-            url: process.env.DB_URL ?? '',
-        });
-        await client.execute({
-            sql: 'INSERT INTO contact(name, email, reason, notes) VALUES(?, ?, ?, ?)',
-            args: [name, email, reason, notes],
-        });
-    } catch {
-        ok = false;
-    }
-    if (client) {
-        client.close();
-    }
-    if (ok) {
-        redirect(
-            `/thanks/?name=${encodeURIComponent(name)}`,
-        );
-    }
+export async function insertContact(
+  formData: FormData,
+) {
+  const parsedResult = contactSchema.safeParse(
+    Object.fromEntries(formData),
+  );
+  if (!parsedResult.success) {
+    return;
+  }
+  const { name, email, reason, notes } =
+    parsedResult.data;
+  let client: Client | undefined;
+  let ok = true;
+  try {
+    console.log(parsedResult)
+    client = createClient({
+      url: process.env.DB_URL ?? '',
+    });
+    await client.execute({
+      sql: 'INSERT INTO contact(name, email, reason, notes) VALUES (?, ?, ?, ?)',
+      args: [name, email, reason, notes ?? null],
+    });
+  } catch {
+    console.log('error insert contact')
+    ok = false;
+  }
+  if (client) {
+    client.close();
+  }
+  if (ok) {
+    redirect(
+      `/thanks/?name=${encodeURIComponent(name)}`,
+    );
+  }
 }
